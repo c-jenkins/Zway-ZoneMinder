69fe7d9d58e2e45ffe0964239700e2a5c6575725
Chris Jenkins <chris.jenkins@blackbaud.com>
2016-12-12 21:31:21 -0600

    cleanup

--- index.js
+++ index.js
@@ -25,42 +25,42 @@ ZoneMinder.prototype.init = function (config) {
     var service = config.zm_port == '443' ? "https" : "http";
     self.baseUrl = service + "://" + config.zm_host + ":" + config.zm_port;
 
-    this.log = function (message) {
-        console.log('[ZoneMinder] ' + message);
-    };
-
-    this.authenticate = function (config, baseUrl) {
-        var args = config.zm_username + " " + config.zm_password + " " + baseUrl;
-        return system("/opt/z-way-server/automation/userModules/ZoneMinder/authenticateZoneMinder.sh",
-            config.zm_username, config.zm_password, baseUrl);
-    }
-
-    this.getMonitors = function (baseUrl) {
-        http.request({
-            url: baseUrl + "/zm/api/monitors.json",
-            method: "GET",
-            async: true,
-            headers: {
-              "Cookie": self.authCookie
-            },
-            success: function (response) {
-                self.log("Monitors data collected");
-                self.configureMonitors(response.data);
-            },
-            error: function (response) {
-                self.log("Error when getting monitors (" + response.status + ")");
-            }
-        });
-    }
-
-    self.configureMonitors = function (monitorConfig) {
-        monitorConfig.monitors.forEach(function (monitor) {
-            self.log("Monitor " + Monitor.Id + ", State: " + Monitor.Function);
-        });
-    }
-
     self.authCookie = self.authenticate(config, self.baseUrl)[1];
     self.getMonitors(self.baseUrl);
 
 };
+
+ZoneMinder.prototype..log = function (message) {
+    console.log('[ZoneMinder] ' + message);
+};
+
+ZoneMinder.prototype..authenticate = function (config, baseUrl) {
+    return system("/opt/z-way-server/automation/userModules/ZoneMinder/authenticateZoneMinder.sh",
+        config.zm_username, config.zm_password, baseUrl);
+}
+
+ZoneMinder.prototype..getMonitors = function (baseUrl) {
+    http.request({
+        url: baseUrl + "/zm/api/monitors.json",
+        method: "GET",
+        async: true,
+        headers: {
+            "Cookie": self.authCookie
+        },
+        success: function (response) {
+            self.log("Monitors data collected");
+            self.configureMonitors(response.data);
+        },
+        error: function (response) {
+            self.log("Error when getting monitors (" + response.status + ")");
+        }
+    });
+}
+
+ZoneMinder.prototype..configureMonitors = function (monitorConfig) {
+    monitorConfig.monitors.forEach(function (m) {
+        self.log("Monitor " + m.Monitor.Id + ", State: " + m.Monitor.Function);
+    });
+}
+
 
